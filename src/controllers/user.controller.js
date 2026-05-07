cb729e0d83ef2da8f8d63a769fed7f485e1eb7f5
abdul mateen <sp23-bse-002@cuilahore.edu.pk>
2026-05-08 01:42:01 +0500

    added user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,66 @@
 import { asynchandler } from "../utils/asynchandler.js";
+import { ApiError } from  "../utils/apierror.js";
+import {User} from "../models/user.model.js";
+import {cloudinaryUpload, uploadtocloudinry} from "../utils/cloudinary.js";
+import {Apiresponse} from "../utils/apiresponse.js";
+
 
 const registerUser = asynchandler( async (req, res) => {
-    res.status(200).json({
-        message : "ok"
+   //get user credentials from front end
+   const {username , fullname , email , passowrd } = req.body
+   //validation
+   if( 
+    [username , fullname , email , passowrd].some((field) => field?.trim() === "")
+   ){
+      throw new ApiError("All fields are required" , 400)
+   }
+   //check if user already exists
+   const user = await User.findOne({
+      $or: [{ email }, { username }]
+   })
+   if(user){
+        throw new ApiError("User already exists" , 400)
+   }
+
+   //check images and avatar
+   const avatarlocalpath = req.files?.avatar[0]?.path;
+   const coverimagelocalpath = req.files?.coverimage[0]?.path;
+   if(!avatarlocalpath){
+        throw new ApiError("Avatar is required" , 400)
+   }
+    const avatarcloudinaryurl = await uploadtocloudinry(avatarlocalpath);
+    const coverimagecloudinaryurl = await uploadtocloudinry(coverimagelocalpath);
+    //create new user in the database
+    const newuser = await User.create({
+        username :username.toLowerCase() , 
+        fullname ,
+        email , 
+        password ,
+        avatar : avatarcloudinaryurl.url,
+        coverimage : coverimagecloudinaryurl?.url || "",
+
     })
+    const checkuser = await User.findById(user._id).select(
+        "-password -refreshtoken" 
+    )
+
+    if(!checkuser){
+        throw new ApiError("User not found" , 500)
+    }
+
+    return res.status(201).json(
+        new Apiresponse(
+            200,
+            checkuser,
+            "user registered successfully"
+        )
+    )
+
+
 })
 
 
 export {registerUser}
+
+
+
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,20 @@
 import {Router } from "express";
+import {Upload} from "../utils/upload.js";
 import { registerUser } from "../controllers/user.controller";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name : "avatar",
+            maxcount : 1
+        },
+        {
+            name : "coverimages",
+            maxcount : 2
+        }
+    ]),
+    registerUser)
 
 export default router;
